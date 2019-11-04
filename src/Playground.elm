5b96a7bd8fc05c57779306817fe08b57d260b65c
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-11-04 17:11:38 +0300

    Add pattern matching examples

--- src/Playground.elm
+++ src/Playground.elm
@@ -1,6 +1,7 @@
 module Playground exposing (..)
 
 import Html exposing (Html, text)
+import Json.Decode exposing (decodeString, int)
 import MyList exposing (..)
 import Regex
 
@@ -293,6 +294,39 @@ list1 =
 list2 : MyList number
 list2 =
     Node 9 Empty
+
+
+resultMap5Example : Result Json.Decode.Error Int
+resultMap5Example =
+    Result.map5 addFiveNumbers
+        (decodeString int "1")
+        (decodeString int "2")
+        (decodeString int "3")
+        (decodeString int "4")
+        (decodeString int "5")
+
+
+addFiveNumbers : Int -> Int -> Int -> Int -> Int -> Int
+addFiveNumbers a b c d e =
+    a + b + c + d + e
+
+
+welcomeMessage : { a | isLoggedIn : Bool, name : String } -> String
+welcomeMessage { isLoggedIn, name } =
+    case isLoggedIn of
+        True ->
+            "Welcome, " ++ name ++ "!"
+
+        False ->
+            "Please log in."
+
+
+type alias User =
+    { name : String
+    , email : String
+    , age : Int
+    , isLoggedIn : Bool
+    }
 
 
 main : Html msg
