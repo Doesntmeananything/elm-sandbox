f69ff3b601deb83fe0b0e24e8824cc2cc834de58
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-11-04 15:02:25 +0300

    Add more fuzz tests

--- tests/FuzzTests.elm
+++ tests/FuzzTests.elm
@@ -1,6 +1,7 @@
-module FuzzTests exposing (addOneTests, addTests)
+module FuzzTests exposing (addOneTests, addTests, arrayGetTests, flipTests, listLengthTests, listReverseTests, multiplyFloatTests, pizzaLeftTests, stringTests)
 
-import Expect exposing (Expectation)
+import Array
+import Expect exposing (Expectation, FloatingPointTolerance(..))
 import Fuzz exposing (..)
 import Random exposing (maxInt, minInt)
 import Test exposing (..)
@@ -28,8 +29,8 @@ add x y =
 addTests : Test
 addTests =
     describe "add"
-        [ fuzz2 int int "adds two given integers" <|
-            \num1 num2 ->
+        [ fuzz (tuple ( int, int )) "adds two given integers" <|
+            \( num1, num2 ) ->
                 add num1 num2
                     |> Expect.equal (num1 + num2)
         ]
@@ -59,4 +60,103 @@ flipTests =
         [ fuzz bool "negates the given boolean value" <|
             \value ->
                 flip value |> Expect.equal (not value)
+        ]
+
+
+multiplyFloat : Float -> Int -> Float
+multiplyFloat x y =
+    x * toFloat y
+
+
+multiplyFloatTests : Test
+multiplyFloatTests =
+    describe "multiplyFloat"
+        [ fuzz2 (floatRange -1.0 1.0) int "multiplies given numbers" <|
+            \x y ->
+                multiplyFloat x y
+                    |> Expect.within (Absolute 0.000000001) (x * toFloat y)
+        ]
+
+
+pizzaLeft : Float -> Float -> Float
+pizzaLeft eatenPercent totalSlices =
+    totalSlices - (eatenPercent * totalSlices)
+
+
+pizzaLeftTests : Test
+pizzaLeftTests =
+    describe "pizzaLeft"
+        [ fuzz2 percentage float "returns remaining pizza slices" <|
+            \eaten total ->
+                pizzaLeft eaten total
+                    |> Expect.within (Absolute 0.000000001) (total - (eaten * total))
+        ]
+
+
+stringTests : Test
+stringTests =
+    describe "The String module"
+        [ describe "String.reverse"
+            [ test "has no effect on a palindrome" <|
+                -- Unit Test - 1
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                    palindrome
+                        |> String.reverse
+                        |> Expect.equal palindrome
+            , test "reverses a known string" <|
+                -- Unit Test - 2
+                \_ ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
+            , fuzz string "restores the original string if you run it again" <|
+                -- Fuzz Test
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
+
+
+listLengthTests : Test
+listLengthTests =
+    describe "List.length"
+        [ fuzz (list int) "never returns a negative value" <|
+            \intList ->
+                intList
+                    |> List.length
+                    |> Expect.atLeast 0
+        ]
+
+
+arrayGetTests : Test
+arrayGetTests =
+    describe "Array.get"
+        [ fuzz (array (intRange -20 20)) "returns Nothing for out of range index" <|
+            \intArray ->
+                let
+                    length =
+                        Array.length intArray
+                in
+                intArray
+                    |> Array.get length
+                    |> Expect.equal Nothing
+        ]
+
+
+listReverseTests : Test
+listReverseTests =
+    describe "List.reverse"
+        [ fuzz2 (list int) int "doesn't remove a member of the list" <|
+            \intList num ->
+                intList
+                    |> List.reverse
+                    |> List.member num
+                    |> Expect.equal (List.member num intList)
         ]
