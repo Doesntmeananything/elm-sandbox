8023431f62e66da4b3b564e771529c07e35e9672
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-11-17 16:56:19 +0300

    Add JSON ports

--- src/PortExamples.elm
+++ src/PortExamples.elm
@@ -3,10 +3,45 @@ port module PortExamples exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Json.Decode exposing (Error(..), Value, decodeValue, string)
 
 
 type alias Model =
-    String
+    { dataFromJS : String
+    , dataToJS : ComplexData
+    , jsonError : Maybe Error
+    }
+
+
+type alias ComplexData =
+    { posts : List Post
+    , comments : List Comment
+    , profile : Profile
+    }
+
+
+type alias Post =
+    { id : Int
+    , title : String
+    , author : Author
+    }
+
+
+type alias Author =
+    { name : String
+    , url : String
+    }
+
+
+type alias Comment =
+    { id : Int
+    , body : String
+    , postId : Int
+    }
+
+
+type alias Profile =
+    { name : String }
 
 
 view : Model -> Html Msg
@@ -14,25 +49,67 @@ view model =
     div []
         [ button [ onClick SendDataToJS ]
             [ text "Send Data to JavaScript" ]
-        , br [] []
-        , br [] []
-        , text ("data received from JavaScript: " ++ model)
+        , viewDataFromJSOrError model
+        ]
+
+
+viewDataFromJSOrError : Model -> Html Msg
+viewDataFromJSOrError model =
+    case model.jsonError of
+        Just error ->
+            viewError error
+
+        Nothing ->
+            viewDataFromJS model.dataFromJS
+
+
+viewError : Error -> Html Msg
+viewError jsonError =
+    let
+        errorHeading =
+            "Couldn't receive data from JavaScript"
+
+        errorMessage =
+            case jsonError of
+                Failure message _ ->
+                    message
+
+                _ ->
+                    "Error: Invalid JSON"
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error: " ++ errorMessage)
+        ]
+
+
+viewDataFromJS : String -> Html msg
+viewDataFromJS data =
+    div []
+        [ br [] []
+        , strong [] [ text "Data received from JavaScript: " ]
+        , text data
         ]
 
 
 type Msg
     = SendDataToJS
-    | ReceivedDataFromJS Model
+    | ReceivedDataFromJS Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SendDataToJS ->
-            ( model, sendData "Hello, JS!" )
+            ( model, sendData model.dataToJS )
 
-        ReceivedDataFromJS data ->
-            ( data, Cmd.none )
+        ReceivedDataFromJS value ->
+            case decodeValue string value of
+                Ok data ->
+                    ( { model | dataFromJS = data }, Cmd.none )
+
+                Err error ->
+                    ( { model | jsonError = Just error }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -40,15 +117,40 @@ subscriptions _ =
     receiveData ReceivedDataFromJS
 
 
-port receiveData : (Model -> msg) -> Sub msg
+port receiveData : (Value -> msg) -> Sub msg
 
 
-port sendData : String -> Cmd msg
+port sendData : ComplexData -> Cmd msg
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "", Cmd.none )
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { dataFromJS = ""
+    , dataToJS = complexData
+    , jsonError = Nothing
+    }
+
+
+complexData : ComplexData
+complexData =
+    let
+        post1 =
+            Author "typicode" "https://github.com/typicode"
+                |> Post 1 "json-server"
+
+        post2 =
+            Author "indexzero" "https://github.com/indexzero"
+                |> Post 2 "http-server"
+    in
+    { posts = [ post1, post2 ]
+    , comments = [ Comment 1 "some comment" 1 ]
+    , profile = { name = "typicode" }
+    }
 
 
 main : Program () Model Msg
